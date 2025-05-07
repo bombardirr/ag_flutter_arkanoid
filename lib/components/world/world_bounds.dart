c6b28111b2e41e13441b6240c014a35861aaecef
Artem Golovin <joe.falange@yandex.ru>
2025-05-07 09:46:55 +0300

    world is resizing now

--- lib/components/world/world_bounds.dart
+++ lib/components/world/world_bounds.dart
@@ -4,33 +4,18 @@ import 'package:flame/components.dart';
 import '../../constants/colors.dart';
 
 class WorldBounds extends RectangleComponent {
-  static const double worldWidth = 200;
-  static const double worldHeight = 300;
+  static const double worldWidth = 400;
+  static const double worldHeight = 600;
 
-  WorldBounds({required Vector2 gameSize})
-    : super(
-        size: Vector2(worldWidth, worldHeight),
-        position: Vector2(
-          (gameSize.x - worldWidth) / 2, // Центрирование по X
-          (gameSize.y - worldHeight) / 2, // Центрирование по Y
-        ),
-        paint:
-            Paint()
-              ..color = GameColors.worldBackground.withValues(
-                alpha: 0.3 * 255, // Конвертируем opacity 0.3 в alpha (0-255)
-              )
-              ..style = PaintingStyle.stroke
-              ..strokeWidth = 4.0,
-      ) {
+  WorldBounds() // Убрали параметр gameSize
+      : super(
+    size: Vector2(worldWidth, worldHeight),
+    position: Vector2.zero(), // Начинаем с (0,0) - позиционирует камера
+    paint: Paint()
+      ..color = GameColors.worldBackground.withAlpha((0.3 * 255).toInt())
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4.0,
+  ) {
     debugMode = true;
-  }
-
-  @override
-  void onGameResize(Vector2 size) {
-    position = Vector2(
-      (size.x - worldWidth) / 2,
-      (size.y - worldHeight) / 2,
-    );
-    super.onGameResize(size);
   }
 }
--- lib/game/arkanoid_game.dart
+++ lib/game/arkanoid_game.dart
@@ -1,16 +1,25 @@
 import 'dart:ui';
 import 'package:ag_flutter_arkanoid/components/paddle/paddle.dart';
 import 'package:ag_flutter_arkanoid/constants/colors.dart';
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import '../components/world/world_bounds.dart';
 
 class ArkanoidGame extends FlameGame {
+
   @override
   Future<void> onLoad() async {
     super.onLoad();
 
     // Добавляем границы мира (фон уровня)
-    add(WorldBounds(gameSize: size));
+    world.add(WorldBounds());
+
+    camera.viewfinder.visibleGameSize = Vector2(WorldBounds.worldWidth, WorldBounds.worldHeight);
+    camera.viewfinder.position = Vector2(
+        WorldBounds.worldWidth / 2,
+        WorldBounds.worldHeight / 2
+    );
+    camera.viewfinder.anchor = Anchor.center;
 
     // Остальные компоненты
     add(Paddle());
